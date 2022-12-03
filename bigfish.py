8ddcff8abf4d1c50a820b9d3a389fc1921e45cb7
Sara Hamidi <103468494+saraHamidiii@users.noreply.github.com>
2022-12-02 23:14:19 -0800

    newer version

--- bigfish.py
+++ bigfish.py
@@ -1,9 +1,4 @@
-# THIS CODE WILL ONLY WORK UNDER THE FOLLOWING ASSUMPTIONS:
-# - you have installed boto3 and tested it according to the lecture training document
-# - you configured your AWS CLI with your IAM access key and secret.
-# - you created a table called "gifts".
-# - you added at least one Item to the gifts table
-# - any Items that you request to view in the code have a attribute called "gifts".
+
 
 import boto3
 import sys
@@ -17,54 +12,52 @@ table = dynamodb.Table('Find_Potty')
 print(table.creation_date_time)
 
 while 1:
-    print("Enter a number\n1) Show all gifts\n2) Add a new gift")
+    print("Enter a number\n1) Show all 'Find Potty' users\n2) Add a new 'Find Potty user'")
 
 
     select = input()
 
     if select == "1":
 
-# WARNING: .scan() finds every item in a table, which is an expensive operation
-# on a KV database. If you frequently need to find all rows in a table, then a KV
-# database might not be the correct storage service for you.
-        all_gifts = table.scan()
 
-        all_occasions = set(i['PottyUser'] for i in all_gifts["Items"])
-        print("Occasion names: ")
-        for o in all_occasions:
+        bname = table.scan()
+
+        allUsers = set(i['PottyUser'] for i in bname["Items"])
+        print("User\'s names: ")
+        for o in allUsers:
             print("\t" + o)
 
 
-        event_name = input("Enter the name of an occasion: ")
-        all_for_event = table.query(KeyConditionExpression=Key("PottyUser").eq(event_name))
-        all_givers = set(i["RestroomName"] for i in all_for_event["Items"])
-        if len(all_givers) > 0:
-            print("Giver names:")
-            for g in all_givers:
+        userName = input("Enter the name of a user: ")
+        allUserNames = table.query(KeyConditionExpression=Key("PottyUser").eq(userName))
+        allBlocations = set(i["RestroomName"] for i in allUserNames["Items"])
+        if len(allBlocations) > 0:
+            print("User\'s names:")
+            for g in allBlocations:
                 print("\t" + g)
 
-            giver = input("Enter the name of a giver:")
-            gift = table.get_item(Key={"PottyUser": event_name, "RestroomName": giver})
-            if "Item" in gift:
-                print(gift["Item"]["gift"])
+            location = input("Restroom Locations they have visited:")
+            bathName = table.get_item(Key={"PottyUser": userName, "RestroomName": location})
+            if "Item" in bathName:
+                print(bathName["Item"]["bathName"])
                 exit(0)
             else:
-                print("No giver with that name.")
+                print("No bathroom locations with that name.")
         else:
-            print("No occasion with that name.")
+            print("No users with that name.")
 
     elif select == "2":
-        all_gifts = table.scan()
-        all_givers = set(i['RestroomName'] for i in all_gifts["Items"])
-        giver_list = []
-        print("Giver names: ")
-        for i in all_givers:
+        bname = table.scan()
+        allBlocations = set(i['RestroomName'] for i in bname["Items"])
+        location_list = []
+        print("Restroom Locations: ")
+        for i in allBlocations:
             print("\t" + i)
-            giver_list.append(i)
-        giver_name = input("Enter the name of the Giver: \t")
-        gift_name = input("Enter the name of the gift: \t")
-        eventino = input ("Enter the event name: \t")
-        table.put_item(Item={"PottyUser" : eventino, "RestroomName" : giver_name, "gift" : gift_name, "date" :  datetime.datetime.now().strftime("%m/%d/%Y, %H:%M:%S")})
+            location_list.append(i)
+        loc_name = input("Enter the name of the restroom location: \t")
+        bath_name = input("Enter the name of the bathroom: \t")
+        uName = input ("Enter the user\'s name: \t")
+        table.put_item(Item={"PottyUser" : uName, "RestroomName" : loc_name, "bathName" : bath_name, "date" :  datetime.datetime.now().strftime("%m/%d/%Y, %H:%M:%S")})
         
     else:
         print("not a valid selection")
